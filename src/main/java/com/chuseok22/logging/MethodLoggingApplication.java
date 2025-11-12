e888fdfa4531b1ef5105d7d78270db21ac312cd6
Chuseok22 <bjh59629@naver.com>
2025-11-12 15:42:08 +0900

    메서드_로깅_커스텀_어노테이션_추가 : feat : project init https://github.com/Chuseok22/method-logging/issues/1

---
+++ src/main/java/com/chuseok22/logging/MethodLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodLoggingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MethodLoggingApplication.class, args);
+  }
+
+}
